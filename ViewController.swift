6540f7fd3745d9635b2cd89fdb151a4d8e5c915f
Eddy Lujan <eddylujann@gmail.com>
2023-09-17 23:59:24 +0200

    Practica 4

---
+++ ViewController.swift
@@ -0,0 +1,35 @@
+//
+//  ViewController.swift
+//  UD3_Practica4
+//
+//  Created by Eddy Donald Chinchay Lujan on 8/8/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+        
+    // MARK: - Properties
+    
+    @IBOutlet weak var label1: UILabel!
+    @IBOutlet weak var label2: UILabel!
+    @IBOutlet weak var label3: UILabel!
+    @IBOutlet weak var label4: UILabel!
+    
+    
+
+    // MARK: - Lifecycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Probando a ocultar la etiqueta 3 para comprobar si funciona el StackView
+        
+        // label3.isHidden = true
+    }
+    
+    // MARK: - Helpers
+    
+    // MARK: - Helpers
+
+}
+
